56f5d7ec9e582d12cd0c16ac8694f7cc543ac117
SamyT-code <71093079+SamyT-code@users.noreply.github.com>
2021-04-29 17:37:21 -0400

    Add files via upload
    Added 3 most interesting projects

---
+++ Forfait_Television/Forfait_Television/Form1.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMealsOnWheels
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnInstructions = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnInstructions
+        '
+        Me.btnInstructions.Location = New System.Drawing.Point(372, 12)
+        Me.btnInstructions.Name = "btnInstructions"
+        Me.btnInstructions.Size = New System.Drawing.Size(124, 34)
+        Me.btnInstructions.TabIndex = 1
+        Me.btnInstructions.Text = "Instructions"
+        Me.btnInstructions.UseVisualStyleBackColor = True
+        '
+        'frmMealsOnWheels
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 22.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(703, 459)
+        Me.Controls.Add(Me.btnInstructions)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.MaximizeBox = False
+        Me.Name = "frmMealsOnWheels"
+        Me.Text = "Meals on Wheels!"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnInstructions As Button
+End Class
---
+++ Forfait_Television/Forfait_Television/Form1.vb
@@ -0,0 +1,12 @@
+﻿'**********************************
+'*Travail : Meals on Wheels!
+'*Programmeur : Samy Touabi
+'*Date de conception : 6 juin 2020
+'**********************************
+Public Class frmMealsOnWheels
+    Private Sub btnInstructions_Click(sender As Object, e As EventArgs) Handles btnInstructions.Click
+
+        MessageBox.Show("Bonjour et bienvenue à l'application Meals on Wheels! Nous sommes ravis de vous servir lors du confinement.")
+
+    End Sub
+End Class
